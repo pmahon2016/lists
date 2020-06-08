33db5f3ec44d57c6b3489359dcedc7b0576dbd1d
Paul Mahon <paulmahon@me.com>
2020-06-08 14:44:31 -0400

    code in video

---
+++ functions.py
@@ -0,0 +1,18 @@
+def print_message(name, language="Python"):
+    print("Hi, my name is " + name + " and my favorite programming language is " + language)
+
+
+print_message(name="John")
+
+
+
+def print_message( name, language):
+    return "Hi, my name is " + name + " and my favorite programming language is " + language
+
+
+
+
+my_var = print_message("Fred", "C++")
+
+
+print(my_var)
